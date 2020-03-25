a426d38eb5e077363ccaeaadf935da0151011e5a
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-03-26 01:50:16 +0530

    chat is running

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when client connects
+io.on('connection', socket => {
+    console.log('New WS Connection.... ');
+
+    socket.emit('message', 'Welcome to chatcord')
+});
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log('Server is running on port' + PORT));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
